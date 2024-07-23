026b8a1a35b4dee82a8dff0a34b5c272a8f92d66
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2024-07-23 20:21:05 +0200

    fix(wip): add sentry logging

--- server/routes/auth/github.get.ts
+++ server/routes/auth/github.get.ts
@@ -5,23 +5,32 @@ export default oauth.githubEventHandler({
     emailRequired: true,
   },
   async onSuccess(event, { user: githubUser }) {
-    const user = await signInController({
-      email: githubUser.email,
-      name: githubUser.name,
-      avatarUrl: githubUser.avatar_url,
-    })
+    try {
+      const user = await signInController({
+        email: githubUser.email,
+        name: githubUser.name,
+        avatarUrl: githubUser.avatar_url,
+      })
 
-    await setUserSession(event, {
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        avatarUrl: user.avatar,
-        role: user.role,
-      },
-      loggedInAt: Date.now(),
-    })
+      await setUserSession(event, {
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatar,
+          role: user.role,
+        },
+        loggedInAt: Date.now(),
+      })
 
-    return sendRedirect(event, '/app')
+      return sendRedirect(event, '/app')
+    }
+    catch (error) {
+      event.context.$sentry.captureException(error)
+      throw createError({
+        message: 'An error occurred while signing in',
+        statusCode: 500,
+      })
+    }
   },
 })
