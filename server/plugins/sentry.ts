877f7b398f6ae7202e5a68991f8b61a1365a4b3c
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2024-07-23 18:55:35 +0200

    chore: add do not handle in sentry plugin

--- server/plugins/sentry.ts
+++ server/plugins/sentry.ts
@@ -1,4 +1,5 @@
 import { Toucan } from 'toucan-js'
+import { H3Error } from 'h3'
 
 export default defineNitroPlugin((nitroApp) => {
   const { sentry } = useRuntimeConfig()
@@ -9,6 +10,13 @@ export default defineNitroPlugin((nitroApp) => {
   }
 
   nitroApp.hooks.hook('error', (err, context) => {
+    // Do not handle 404, 422 and 401 errors
+    if (err instanceof H3Error) {
+      if (err.statusCode === 404 || err.statusCode === 422 || err.statusCode === 401) {
+        return
+      }
+    }
+
     const sentryToucan = new Toucan({
       dsn: sentry.dsn,
       context: context.event,
@@ -18,5 +26,14 @@ export default defineNitroPlugin((nitroApp) => {
     // set some other tags
 
     sentryToucan.captureException(err)
+  })
+
+  nitroApp.hooks.hook('request', (event) => {
+    const sentryToucan = new Toucan({
+      dsn: sentry.dsn,
+      context: event,
+    })
+
+    event.context.$sentry = sentryToucan
   })
 })
