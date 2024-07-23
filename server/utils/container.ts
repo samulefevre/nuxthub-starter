9a71ec8b4a9fbd67b1036b05cd8a17b5b2b2796d
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2024-07-23 21:02:34 +0200

    refactor: container to use getContainer

--- server/utils/container.ts
+++ server/utils/container.ts
@@ -1,3 +1,4 @@
+import type { AwilixContainer } from 'awilix'
 import { createContainer, asClass } from 'awilix'
 import { DrizzleDeleteAccountTokenRepository, DrizzleMagicLinkRepository, DrizzleUserRepository } from '../infrastructure/repositories'
 
@@ -14,28 +15,34 @@ export interface IDependencies {
   imageService: IImageService
 }
 
-const container = createContainer<IDependencies>({
-  strict: true,
-})
+let container: AwilixContainer<IDependencies> | null = null
 
-if (!process.env.NUXT_RESEND_API_KEY) {
-  throw new Error('NUXT_RESEND_API_KEY is not defined')
+export function getContainer(): AwilixContainer<IDependencies> {
+  if (!container) {
+    if (!process.env.NUXT_RESEND_API_KEY) {
+      throw new Error('NUXT_RESEND_API_KEY is not defined')
+    }
+
+    container = createContainer<IDependencies>({
+      strict: true,
+    })
+
+    container.register({
+      userRepository: asClass(DrizzleUserRepository).singleton(),
+      deleteAccountTokenRepository: asClass(DrizzleDeleteAccountTokenRepository).singleton(),
+      magicLinkRepository: asClass(DrizzleMagicLinkRepository).singleton(),
+      emailService: asClass(EmailService).inject(() => ({
+        apiKey: process.env.NUXT_RESEND_API_KEY || '',
+        baseUrl: process.env.NUXT_PUBLIC_BASE_URL || '',
+        fromEmail: process.env.NUXT_EMAILS_FROM_EMAIL || '',
+      })).singleton(),
+      imageService: asClass(ImageService).singleton(),
+    })
+  }
+  return container
 }
 
-container.register({
-  userRepository: asClass(DrizzleUserRepository).singleton(),
-  deleteAccountTokenRepository: asClass(DrizzleDeleteAccountTokenRepository).singleton(),
-  magicLinkRepository: asClass(DrizzleMagicLinkRepository).singleton(),
-  emailService: asClass(EmailService).inject(() => ({
-    apiKey: process.env.NUXT_RESEND_API_KEY || '',
-    baseUrl: process.env.NUXT_PUBLIC_BASE_URL || '',
-    fromEmail: process.env.NUXT_EMAILS_FROM_EMAIL || '',
-  })).singleton(),
-  imageService: asClass(ImageService).singleton(),
-})
-
-export { container }
-
 export function resolve<K extends keyof IDependencies>(key: K): IDependencies[K] {
+  const container = getContainer()
   return container.resolve(key)
 }
