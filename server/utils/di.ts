5e3f37e53543b3a6753cc4623d1e5969ddbb8a71
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2024-07-24 22:28:05 +0200

    fix(wip): remove stric mode

--- server/utils/di.ts
+++ server/utils/di.ts
@@ -1,12 +1,12 @@
 import type { AwilixContainer } from 'awilix'
 import { createContainer, asClass } from 'awilix'
 
-import type { IUserRepository } from '@@/server/application/repositories'
+/* import type { IUserRepository } from '@@/server/application/repositories' */
 
 import { DrizzleUserRepository } from '@@/server/infrastructure/repositories'
 
 export interface IDependencies {
-  userRepository: IUserRepository
+  userRepository: DrizzleUserRepository
 }
 
 let container: AwilixContainer<IDependencies>
@@ -15,7 +15,7 @@ export const useDI = () => {
   if (!container) {
     console.log('Creating DI')
     container = createContainer<IDependencies>({
-      strict: true,
+      // strict: true,
       injectionMode: 'PROXY',
     })
 
