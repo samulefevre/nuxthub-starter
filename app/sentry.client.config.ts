b08998b29a649286bdd93634743626da1bba270b
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2024-08-29 15:27:34 +0200

    fix: sentry config

--- app/sentry.client.config.ts
+++ app/sentry.client.config.ts
@@ -7,4 +7,5 @@ Sentry.init({
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV,
 })
--- public/instrument.server.mjs
+++ public/instrument.server.mjs
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/nuxt";
 
 Sentry.init({
-  dsn: "https://ed55a0a67026ea971faa1b2c1c3e272e@o4506983804698624.ingest.us.sentry.io/4507651974889472",
+  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
   enabled: true,
   debug: true,
   tracesSampleRate: 1,
-  environment: 'development',
+  environment: process.env.NODE_ENV,
 });
